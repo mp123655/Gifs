c845d1f407f0c2ed3b1b94c11ac875c0bbeed12e
M P <maria.perz@wp.pl>
2020-02-16 13:04:14 +0100

    Added favorites

--- src/main/java/pl/akademiakodu/gifs/controller/MainController.java
+++ src/main/java/pl/akademiakodu/gifs/controller/MainController.java
@@ -3,11 +3,14 @@ package pl.akademiakodu.gifs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.gifs.model.Gif;
 import pl.akademiakodu.gifs.repository.GifRepository;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -16,9 +19,20 @@ public class MainController {
     GifRepository gifRepository;
 
     @GetMapping("/")
-    @ResponseBody
-    public String hello(){
-        return "dziala";
+
+    public String hello(ModelMap modelMap){
+
+        //praca z Thymlifem:
+        //1. Wyciagniecie gifow
+
+        List<Gif> gifList= gifRepository.getGifs();
+
+        //2. przekazanie do view
+        modelMap.put("gifs",gifList); // laczy frontend z backendem. jako klucz nazwa z forntendu, wartosc z backendu
+
+        //3. zwrocenie widoku
+
+        return "home"; // nazwa pliku html, ale bez rozszerzenia
     }
 
     @GetMapping("/showgifsinbrowser")
@@ -27,5 +41,17 @@ public class MainController {
         return gifRepository.getGifsNames();
     }
 
+    @GetMapping("/favorites")
+    public String favoritesGifts(ModelMap modelMap){
+        //1. Wciagniecie gifs
+        List<Gif> favoriteGifs=gifRepository.getFavoritesGifs();
+
+        //2. Przekazanie do view
+
+        modelMap.put("gifs",favoriteGifs);
+
+        //3. zwrocenie widoku
+        return "favorites";
+    }
 
 }
--- src/main/java/pl/akademiakodu/gifs/repository/GifRepository.java
+++ src/main/java/pl/akademiakodu/gifs/repository/GifRepository.java
@@ -6,31 +6,43 @@ import pl.akademiakodu.gifs.model.Gif;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class GifRepository {
 
     private static List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif ("android-explosion", "mols", true,1),
-            new Gif ("ben-and-mike", "mika", true,2),
-            new Gif ("book-dominos", "mem", true,0),
-            new Gif ("compiler-bot", "bot", true,1),
-            new Gif ("cowboy-coder", "code", true,1),
-            new Gif ("infinite-andrew", "andrew", true,2)
+            new Gif("android-explosion", "mols", true, 1),
+            new Gif("ben-and-mike", "mika", true, 2),
+            new Gif("book-dominos", "mem", true, 0),
+            new Gif("compiler-bot", "bot", true, 1),
+            new Gif("cowboy-coder", "code", true, 1),
+            new Gif("infinite-andrew", "andrew", false, 2)
 
     );
 
-    public List<Gif> getGifs(){
+    public List<Gif> getGifs() {
         return ALL_GIFS;
     }
 
-    public String getGifsNames(){
-        String result="";
+    public String getGifsNames() {
+        String result = "";
         for (Gif gif : ALL_GIFS) {
-            result += gif.getName();}
-        return result;
+            result += gif.getName();
         }
+        return result;
     }
+
+    public List<Gif> getFavoritesGifs() {
+
+        return ALL_GIFS.stream()
+                .filter(Gif::getFavorite)
+            .collect(Collectors.toList());
+
+    }
+
+
+}
 
 
 
